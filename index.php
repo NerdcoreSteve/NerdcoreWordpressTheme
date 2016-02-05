5406f4fd0559b2401026ec4258352f7d73bc8a23
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2016-02-04 20:58:50 -0700

    looks like a theme alright

--- index.php
+++ index.php
@@ -2,12 +2,10 @@
     get_header();
 
     if(have_posts()):
-        while(have_posts()):the_post();
-            ?>
-                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2> 
-                <p><?php the_content(); ?></h2>
-            <?php
-        endwhile;
+        while(have_posts()):the_post();?>
+            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2> 
+            <p><?php the_content(); ?></h2>
+        <?php endwhile;
     else:
         echo "<p>No Content Found</p>";
     endif;
